c70069017522e9b274e8d5352e14ad69b9f472c8
Ryan Sundberg <ryan.sundberg@gmail.com>
2021-01-24 11:53:31 -0800

    Helios: add key id api and require sudo for admin interfaces

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -2,7 +2,7 @@
 
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch,
     traits::Get, traits::Vec, sp_std::convert::TryInto};
-use frame_system::ensure_signed;
+use frame_system::{ensure_signed, ensure_root};
 
 #[cfg(test)]
 mod mock;
@@ -14,13 +14,17 @@ mod tests;
 pub trait Trait: frame_system::Trait {
     /// Because this pallet emits events, it depends on the runtime's definition of an event.
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	// Maps key id to the enabled/disabled state
+	//type AuthorizedKeys: StoredMap<u64, bool>;
 }
 
 // The pallet's runtime storage items.
 // https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
     trait Store for Module<T: Trait> as TemplateModule {
-        MaxTransactionSize get(fn size): u32 = 65536; // 64kb
+        MaxTransactionSize get(fn max_transaction_size): u32 = 65536; // 64kb
+        LastTransactionId get(fn last_transaction_id): u64 = 0;
+		AuthorizedKeys get(fn authorized_keys): map hasher(identity) u32 => bool;
     }
 }
 
@@ -28,7 +32,7 @@ decl_storage! {
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
     pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-        Transaction(AccountId, u32, Vec<u8>), // [ accountId, version, data ]
+        Transaction(AccountId, u64, u32, u32, Vec<u8>), // [ account_id, tx_id, version, key_id, data ]
     }
 );
 
@@ -38,20 +42,18 @@ decl_error! {
         InvalidTransactionVersion, // unsupported transaction version
         EmptyTransaction, // data is 0
         TransactionOverflow, // data exceeds max transaction size
+		InvalidKeyId, // Key is disabled or not authorized
     }
 }
 
-// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// These functions materialize as "extrinsics", which are often compared to transactions.
-// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         type Error = Error<T>;
         fn deposit_event() = default;
 
         // Add a datalog transaction to the database.
-        #[weight = 10_000 + T::DbWeight::get().reads_writes(1, 0)]
-        pub fn transact(origin, version: u32, data: Vec<u8>) -> dispatch::DispatchResult {
+        #[weight = 10_000 + T::DbWeight::get().reads_writes(3, 2)]
+        pub fn transact(origin, version: u32, key_id: u32, data: Vec<u8>) -> dispatch::DispatchResult {
             let who = ensure_signed(origin)?;
             let max_size = MaxTransactionSize::get();
             let size = data.len();
@@ -64,8 +66,29 @@ decl_module! {
             else {
                 match version {
                     0 => {
-                        Self::deposit_event(RawEvent::Transaction(who, version, data));
-                        Ok(())
+                        let valid_key_id;
+                        if key_id == 0 {
+                            if AuthorizedKeys::contains_key(key_id) {
+                                valid_key_id = AuthorizedKeys::get(key_id);
+                            }
+                            else {
+                                // key id 0 is the null key (unencrypted data)
+                                // default to unencrypted
+                                valid_key_id = true;
+                            }
+                        }
+                        else {
+                            valid_key_id = AuthorizedKeys::get(key_id);
+                        }
+						if valid_key_id {
+							let tx_id = LastTransactionId::get() + 1;
+							LastTransactionId::set(tx_id);
+							Self::deposit_event(RawEvent::Transaction(who, tx_id, version, key_id, data));
+							Ok(())
+						}
+                        else {
+                            Err(Error::<T>::InvalidKeyId)?
+                        }
                     },
                     _ => Err(Error::<T>::InvalidTransactionVersion)?,
                 }
@@ -75,9 +98,25 @@ decl_module! {
         // Change the max transaction size
         #[weight = 10_000 + T::DbWeight::get().reads_writes(0, 1)]
         pub fn admin_set_max_transaction_size(origin, size: u32) -> dispatch::DispatchResult {
-            // TODO ensure origin has sudo
+            ensure_root(origin)?;
             MaxTransactionSize::set(size);
             Ok(())
         }
+
+		// Add an authorized key id
+		#[weight = 10_000 + T::DbWeight::get().reads_writes(0, 1)]
+		pub fn admin_enable_key(origin, key_id: u32) -> dispatch::DispatchResult {
+            ensure_root(origin)?;
+			AuthorizedKeys::insert(key_id, true);
+			Ok(())
+		}
+
+		// Remove an authorized key id
+		#[weight = 10_000 + T::DbWeight::get().reads_writes(0, 1)]
+		pub fn admin_disable_key(origin, key_id: u32) -> dispatch::DispatchResult {
+            ensure_root(origin)?;
+			AuthorizedKeys::insert(key_id, false);
+			Ok(())
+		}
     }
 }
--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,25 +1,81 @@
 use crate::{Error, mock::*};
-use frame_support::{assert_ok, assert_noop};
+use frame_support::{assert_ok, assert_noop, traits::Vec, dispatch::DispatchError};
+use frame_system::{RawOrigin};
 
 #[test]
-fn it_works_for_default_value() {
+fn test_transaction_ids() {
 	new_test_ext().execute_with(|| {
+        let data1 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0a];
+        let data2 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0b, 0x05];
+        let data3 = vec![0x0b, 0x0e, 0x0e, 0x0f];
 		// Dispatch a signed extrinsic.
-		//assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		//assert_eq!(TemplateModule::something(), Some(42));
+		assert_ok!(TemplateModule::transact(Origin::signed(1), 0, 0, data1));
+		assert_ok!(TemplateModule::transact(Origin::signed(1), 0, 0, data2));
+		assert_ok!(TemplateModule::transact(Origin::signed(2), 0, 0, data3));
+        assert_eq!(TemplateModule::last_transaction_id(), 3);
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn test_empty_transaction_fails() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-        /*
+        let empty_data = Vec::new();
+        assert_noop!(
+            TemplateModule::transact(Origin::signed(1), 0, 0, empty_data),
+            Error::<Test>::EmptyTransaction
+        );
+	});
+}
+
+#[test]
+fn test_max_transaction_size() {
+	new_test_ext().execute_with(|| {
+        let data1 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0a];
+        let data2 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0b, 0x05];
+        let data3 = vec![0x0b, 0x0e, 0x0e, 0x0f];
+        assert_noop!(
+            TemplateModule::admin_set_max_transaction_size(Origin::signed(1), 8),
+            DispatchError::BadOrigin
+        );
+        assert_ok!(TemplateModule::admin_set_max_transaction_size(RawOrigin::Root.into(), 4));
 		assert_noop!(
-			TemplateModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
-		);
-        */
+            TemplateModule::transact(Origin::signed(1), 0, 0, data1),
+            Error::<Test>::TransactionOverflow
+        );
+            
+		assert_noop!(
+            TemplateModule::transact(Origin::signed(1), 0, 0, data2),
+            Error::<Test>::TransactionOverflow
+        );
+		assert_ok!(TemplateModule::transact(Origin::signed(2), 0, 0, data3));
+        assert_eq!(TemplateModule::last_transaction_id(), 1);
+	});
+}
+
+#[test]
+fn test_transaction_keys() {
+	new_test_ext().execute_with(|| {
+        let data1 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0a];
+        let data2 = vec![0x0b, 0x00, 0x00, 0x0b, 0x0b, 0x05];
+        let data3 = vec![0x0b, 0x0e, 0x0e, 0x0f];
+        let data4 = vec![0x0b, 0x0e, 0x0e, 0x0f];
+        let data5 = vec![0x0b, 0x0e, 0x0e, 0x0f];
+		assert_ok!(TemplateModule::transact(Origin::signed(1), 0, 0, data1));
+		assert_noop!(
+            TemplateModule::transact(Origin::signed(1), 0, 1, data2),
+            Error::<Test>::InvalidKeyId
+        );
+        assert_ok!(TemplateModule::admin_enable_key(RawOrigin::Root.into(), 1));
+		assert_ok!(TemplateModule::transact(Origin::signed(1), 0, 1, data3));
+        assert_ok!(TemplateModule::admin_disable_key(RawOrigin::Root.into(), 1));
+		assert_noop!(
+            TemplateModule::transact(Origin::signed(1), 0, 1, data4),
+            Error::<Test>::InvalidKeyId
+        );
+        assert_ok!(TemplateModule::admin_disable_key(RawOrigin::Root.into(), 0));
+		assert_noop!(
+            TemplateModule::transact(Origin::signed(1), 0, 0, data5),
+            Error::<Test>::InvalidKeyId
+        );
 	});
 }
